b6797b1ceda5fa0fe48192c1be2cfd4055d8a413
Rodolphe Farrando <rodolphe.farrando@epfl.ch>
2017-12-01 10:08:11 +0100

    Conflit testeigen

--- code/test_eigen.cpp
+++ code/test_eigen.cpp
@@ -5,9 +5,9 @@ using namespace Eigen;
 
 int main()
 {
-    MatrixXf A(n,n);
+    MatrixXf A(3,3);
     Vector3f b;
-    A << 3,6,14, 6,14,36, 14,36,98;
+    A << 3,6,14,6,14,36,14,36,98;
     b << 36, 98, 276;
     cout << "Here is the matrix A:\n" << A << endl;
     cout << "Here is the vector b:\n" << b << endl;
