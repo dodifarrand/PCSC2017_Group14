63cdfb974d7300387eafab74e52ea90ddb67921e
Rodolphe Farrando <rodolphe.farrando@epfl.ch>
2017-12-03 16:06:46 +0100

    Piecwise quadratic ok

--- code/leastsquares.cpp
+++ code/leastsquares.cpp
@@ -4,6 +4,7 @@ using namespace std;
 using namespace Eigen;
 
 void CalculateCoeff(int *x, int *y, int pointNb, int polDegree);
+void pieceWiseLinear(int *x, int *y, int pointNb,int polDegree);
 
 int main()
 {
@@ -14,17 +15,18 @@ int main()
     xi[2] = 3;
     xi[3] = 4;
     xi[4] = 5;
-    xi[5] = 6;
-    yi[0] = 2;
-    yi[1] = 5;
-    yi[2] = 12;
+    //xi[5] = 6;
+    yi[0] = 1;
+    yi[1] = 10;
+    yi[2] = 5;
     yi[3] = 18;
     yi[4] = 40;
-    yi[5] = 46;
+    //yi[5] = 46;
     //int yi[3] = {1,4,9};
     //VectorXd y;
     //MatrixXd A;
-    CalculateCoeff(xi,yi,6,5);
+    //CalculateCoeff(xi,yi,2,2);
+    pieceWiseLinear(xi, yi, 5,2);
 }
 
 void CalculateCoeff(int *x, int *y, int pointNb, int polDegree){
@@ -52,4 +54,63 @@ void CalculateCoeff(int *x, int *y, int pointNb, int polDegree){
     cout << "Here is the vector b:\n" << b << endl;
     VectorXd a = A.colPivHouseholderQr().solve(b);
     cout << "The solution is:\n" << a << endl;
+}
+
+void pieceWiseLinear(int *x, int *y, int pointNb,int polDegree){
+    int unknownNb = (pointNb-1)*(polDegree+1);
+    MatrixXd A(unknownNb,unknownNb);
+    VectorXd b(unknownNb);
+    //b<<y[0],y[1],y[1],y[2],0;
+    for(int i = 0;i<unknownNb;i++){
+        for(int j = 0;j<unknownNb;j++){
+            A(i,j)=0;
+        }
+        b(i)=0;
+    }
+    int j = 0;
+    int f = 0;
+    for(int i = 0;i<(pointNb-1)*2;i = i+2){
+        A(i,j) = pow(x[f],2);
+        A(i,j+1) = pow(x[f],1);
+        A(i,j+2) = pow(x[f],0);
+        A(i+1,j) = pow(x[f+1],2);
+        A(i+1,j+1) = pow(x[f+1],1);
+        A(i+1,j+2) = pow(x[f+1],0);
+        b(i)=y[f];
+        b(i+1)=y[f+1];
+        j = j+3;
+        f = f+1;
+    }
+    A((pointNb-1)*2,0)=1;
+    //A((pointNb-1)*2,3)=-1;
+    //A((pointNb-1)*2+1,3)=1;
+    //A((pointNb-1)*2+1,6)=-1;
+    j = 0;
+    f = 1;
+    for(int i = (pointNb-1)*2+1;i<(unknownNb);i++){
+        A(i,j)=2*x[f];
+        A(i,j+1)=1;
+        A(i,j+3)=-2*x[f];
+        A(i,j+4)=-1;
+        j = j+3;
+        f = f+1;
+    }
+    //A(0,0)=x[0];
+    //A(0,1)=1;
+    //A(1,0)=x[1];
+    //A(1,1)=1;
+    //A(2,2)=pow(x[1],2);
+    //A(2,3)=x[1];
+    //A(2,4)=1;
+    //A(3,2)=pow(x[2],2);
+    //A(3,3)=x[2];
+    //A(3,4)=1;
+    //A(4,0)=1;
+    //A(4,2)=-2*x[1];
+    //A(4,3)=-1;
+    cout << "Here is the matrix A:\n" << A << endl;
+    cout << "Here is the vector b:\n" << b << endl;
+    VectorXd a = A.colPivHouseholderQr().solve(b);
+    cout << "The solution is:\n" << a << endl;
+
 }
