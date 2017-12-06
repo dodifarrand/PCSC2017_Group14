413df029f507fa37418b4199624cd863318dce50
Anouk <anoukallenspach@gmail.com>
2017-12-06 16:35:15 +0100

    save before  implementing symbol or hash table

--- code/test_parser.cpp
+++ code/test_parser.cpp
@@ -17,30 +17,76 @@ enum Token {
 };
 class Scanner
 {
+
 public:
-    Scanner (char const * buf); // initialize scanner
+    Scanner (char const * buffer); // initialize scanner to scan the buffer that is passed
 private:
-    char const * const buffer;
+    char const * const line;  // constant pointer to the constant char line that is read (we will read only one function)
 };
 
-Scanner::Scanner (char const * buf) : buffer (buf)
-{
-    std::cout << "Scanner with \"" << buf << "\""  << std::endl; // only way to include " in a string is to put a backslash
+Scanner::Scanner (char const * buffer) : line (buffer)  // set line to buffer when scanner object is constructed
+                                                        // not possible to do otherwise because we cannot change a constant
+{   // could also be empty
+    std::cout << "Scanner with \"" << line << "\""  << std::endl; // only way to include " in a string is to put a backslash
 
 }
 
+// class to parse string containing the function
+// this will be used by the scanner in order to parse the token
+// the symbol table will be used by the parser in order to evaluate the
+class Parser
+{
+public:
+    // constructor
+    Parser();
+    // destructor
+    ~Parser();
 
+    // function to evaluate what is parsed should tell us if it was successful or not
+    //
+    int Eval();
+
+
+};
+
+Parser::Parser() {
+    std::cout << "Create object of type parser" << std::endl;
+}
+
+
+Parser::~Parser() {
+    std::cout << "Destroy the parser" << std::endl;
+}
+
+int Parser::Eval() {
+    std::cout << "evaluate the tree" << std::endl;
+    return -1;
+}
+
+// class of symbols to make the link between variable names and values
+// In computing, a hash table (hash map) is a data structure which implements
+// an associative array abstract data type, a structure that can map keys to
+// values. A hash table uses a hash function to compute an index into an array
+// of buckets or slots, from which the desired value can be found
 
 class SymbolTable
 {
 public:
     // constructor
-    explicit SymbolTable(int size); // should not be called by compiler implicitly, only when we really want a symbol table
+    SymbolTable(); // should not be called by compiler implicitly, only when we really want a symbol table
     ~SymbolTable(); // destructor
-    int
+
 
 };
+
+
+
+
+
 int main(int argc, char* argv[]){
+
+
+    // different tokens that can be encountered
     Token r;
     switch(r)
     {
