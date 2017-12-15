bcd2652fcd75f18af91225369798aa5a419c17b0
Anouk <anoukallenspach@gmail.com>
2017-12-15 14:09:14 +0100

    add do

--- Doc/html/search/searchdata.js
+++ Doc/html/search/searchdata.js
@@ -1,12 +1,30 @@
 var indexSectionsWithContent =
 {
+  0: "acdefgimnprstwxy~",
+  1: "aefip",
+  2: "acefipt",
+  3: "acefgimprswxy~",
+  4: "dntxy",
+  5: "a"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "related"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Friends"
 };
 
