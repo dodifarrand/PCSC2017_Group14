b63942217353478eb327dc81fb05eb574f547fe1
Anouk <anoukallenspach@gmail.com>
2017-12-15 13:17:16 +0100

    finished test main for function approximation testing

--- code/TestFunctionApprox.cpp
+++ code/TestFunctionApprox.cpp
@@ -8,4 +8,23 @@
  *      class to read the configuration file named config.csv
  */
 
+#include "FunctionApprox.h"
 
+// specify your own function
+
+// first generate a .csv file named testing
+// testing.csv contains: the name of your function (MyOwnFunction), beginning of interval (0), end of interval,
+// number of points to generate,type of approximation,degree of approximation
+
+double MyOwnFunction(double x){
+    return exp(x/2)-pow(x,3);
+}
+
+
+int main(int argc, char* argv[]) {
+    FunctionApprox fuctionApprox1("testing.csv", &MyOwnFunction);
+
+
+    return 0;
+
+}
