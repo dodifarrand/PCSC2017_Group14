22a1004e70fa67f3e121d11e82057e8bc887914f
Anouk <anoukallenspach@gmail.com>
2017-12-15 13:06:59 +0100

    new comments and added test file for function approximation

---
+++ code/TestFunctionApprox.cpp
@@ -0,0 +1,11 @@
+
+/**
+ * @file
+ * TestFunctionApprox.cpp
+ *
+ *  Created on: Dec 15, 2017
+ *      Author: Created by Anouk Allenspach
+ *      class to read the configuration file named config.csv
+ */
+
+
